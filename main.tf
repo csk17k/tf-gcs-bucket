9d49a8f7777b39d13f65bd9c65cd9341a95afdb4
csk17k <84774760+csk17k@users.noreply.github.com>
2021-06-17 20:22:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 provider "google" {
 }
-resource "google_storage_bucket" "default" {
+resource "google_storage_bucket" "mypic" {
   name = var.bucket_name
   project = var.project_id
   storage_class = var.storage_class
   location = var.bucket_location
+  source = "/images/nature/garden-tiger-moth.jpg"
+  bucket = "image-store"
 }
